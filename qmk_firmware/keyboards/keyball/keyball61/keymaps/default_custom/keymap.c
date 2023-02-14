6c0311b1a9d6d58c0cbd9edaa3340a1c558f661c
takira <ca.fie.0220@gmail.com>
2023-02-14 17:07:17 +0900

    Update default_custom

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default_custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default_custom/keymap.c
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_EQL   ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSHT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MO(3)    ,
-    MO(3)    , _______  , KC_LALT  , KC_LGUI  ,LT(1,KC_LNG2),KC_SPC , MO(3)    ,              KC_ENT ,LT(2,LNG1), _______  , _______  , _______  , KC_BSLS  , KC_GRV
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MO(3)    ,
+    MO(3)    , _______  , KC_LALT  , KC_LGUI  ,LT(1,KC_LNG2),KC_SPC , MO(3)    ,              KC_ENT ,LT(2,KC_LNG1), _______  , _______  , _______  , KC_BSLS  , KC_GRV
   ),
 
   [1] = LAYOUT_universal(
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , MEH(KC_7), MEH(KC_8), MEH(KC_9), _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  MEH(KC_Y), MEH(KC_U), MEH(KC_I), MEH(KC_O), _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  LGUI(KC_W),LGUI(KC_LEFT),LGUI(KC_N),LGUI(KC_RGHT), _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , LSG(KC_T),LCS(KC_TAB),LGUI(KC_T),LCTL(KC_TAB), _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , LSG(KC_T),RCS(KC_TAB),LGUI(KC_T),LCTL(KC_TAB), _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
   ),
 
