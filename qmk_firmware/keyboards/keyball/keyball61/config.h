d0e2eb3cd7a7a0f5d714184020eef4154670f539
takira <ca.fie.0220@gmail.com>
2023-11-04 09:28:40 +0900

    update tapping_term from 150 to 120

--- qmk_firmware/keyboards/keyball/keyball61/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/config.h
@@ -84,6 +84,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_TIME 90
 #define AUTO_MOUSE_DELAY 5
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 120
 
 #define TAPPING_FORCE_HOLD
