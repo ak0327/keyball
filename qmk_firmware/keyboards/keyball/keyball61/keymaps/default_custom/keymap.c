4de5577eba2505bcd00a3d1ffccc0c15bd31d55e
takira <ca.fie.0220@gmail.com>
2023-11-07 16:33:53 +0900

    refactor

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/default_custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/default_custom/keymap.c
@@ -23,10 +23,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
-    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_EQL   ,
-    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MO(3)    ,
+    KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6        , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
+    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y        , KC_U     , KC_I     , KC_O     , KC_P     , KC_EQL   ,
+    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H        , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,              KC_RBRC, KC_N        , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , MO(3)    ,
     MO(3)    , _______  , KC_LALT  , KC_LGUI  ,LT(1,KC_LNG2),KC_SPC , MO(3)    ,              KC_ENT ,LT(2,KC_LNG1), _______  , _______  , _______  , KC_BSLS  , KC_GRV
   ),
 
@@ -39,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT_universal(
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , MEH(KC_7), MEH(KC_8), MEH(KC_9), _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  MEH(KC_Y), MEH(KC_U), MEH(KC_I), MEH(KC_O), _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______   , MEH(KC_7)   , MEH(KC_8), MEH(KC_9)   , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                  MEH(KC_Y) , MEH(KC_U)   , MEH(KC_I), MEH(KC_O)   , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  LGUI(KC_W),LGUI(KC_LEFT),LGUI(KC_N),LGUI(KC_RGHT), _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , LSG(KC_T),RCS(KC_TAB),LGUI(KC_T),LCTL(KC_TAB), _______  , _______  ,
-    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , LSG(KC_T) ,RCS(KC_TAB)  ,LGUI(KC_T),LCTL(KC_TAB) , _______  , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______   , _______     , _______  , _______     , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    _______  , CPI_I100 , CPI_I1K  , SCRL_DVI , _______  , _______  ,                                  _______  , KC_BTN1  , KC_BTN3  , KC_BTN2  , _______  , _______  ,
    _______  , CPI_D100 , CPI_D1K  , SCRL_DVD , _______  , _______  , KBC_SAVE ,            _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
    _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
- ),
+  ),
 
  [4] = LAYOUT_universal(
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , _______  , _______  , _______  , _______  , _______  ,
@@ -60,7 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  _______  , KC_BTN1  , KC_BTN3  , KC_BTN2  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , _______  , _______  , _______  , _______  , _______
- ),
+  ),
+
 };
 // clang-format on
 
