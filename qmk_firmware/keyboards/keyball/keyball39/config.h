8954f2d767119424309cc9929e54803b766d9be7
takira <ca.fie.0220@gmail.com>
2023-11-26 14:43:52 +0900

    update keymap of keyball39:custom

--- qmk_firmware/keyboards/keyball/keyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/config.h
@@ -85,7 +85,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_TIME 90
 #define AUTO_MOUSE_DELAY 5
 
-#define TAPPING_TERM 130
+#define TAPPING_TERM 150
+#define PERMISIVE_HOLD
 
 #define TAPPING_FORCE_HOLD
 
